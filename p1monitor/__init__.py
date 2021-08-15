7e46c5b11e73d5a26ae09db0aa20230b6bc6757c
Klaas Schoute <klaas_schoute@hotmail.com>
2021-08-16 01:51:40 +0200

    🚚 Rename the project (#3)
    * 🚚 Rename the project
    
    * Start with new cache
    
    * Revert latest change
    
    * Revert change to create new cache
    
    * Change cache keys for release workflow

--- p1monitor/__init__.py
+++ p1monitor/__init__.py
@@ -1,7 +1,7 @@
 """Asynchronous Python client for the P1 Monitor API."""
 
 from .models import Phases, Settings, SmartMeter
-from .p1_monitor import P1Monitor, P1MonitorConnectionError, P1MonitorError
+from .p1monitor import P1Monitor, P1MonitorConnectionError, P1MonitorError
 
 __all__ = [
     "P1Monitor",
